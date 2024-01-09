251566635d292f57212ea8d7edc16ad313174ac2
23nvalentinov <148052274+23nvalentinov@users.noreply.github.com>
2024-01-09 20:05:21 +0100

    Add files via upload

--- ArbolBinarioAleatorio/arbolcrece.py
+++ ArbolBinarioAleatorio/arbolcrece.py
@@ -1,6 +1,6 @@
-import random as rndm
+import random
 
-class MiNodo: 
+class Nodo: 
     def __init__(self, valor): 
         self.valor = valor 
         self.izquierda = None
@@ -10,25 +10,25 @@ def arbol_binario_aleatorio(tamano):
     if tamano == 0: 
         return None
 
-    tamano_izquierda = rndm.randint(0, tamano-1) 
+    tamano_izquierda = random.randint(0, tamano-1) 
     tamano_derecha = tamano - 1 - tamano_izquierda 
 
     subarbol_izquierdo = arbol_binario_aleatorio(tamano_izquierda) 
     subarbol_derecho = arbol_binario_aleatorio(tamano_derecha) 
  
-    raiz = MiNodo(rndm.randint(0, 10)) 
+    raiz = Nodo(random.randint(0, 10)) 
  
     raiz.izquierda = subarbol_izquierdo 
     raiz.derecha = subarbol_derecho 
 
     return raiz 
 
-def imprimir_arbol(nodo, nivel=0): 
+def print_arbol(nodo, nivel=0): 
     if nodo is not None: 
-        imprimir_arbol(nodo.derecha, nivel + 1) 
+        print_arbol(nodo.derecha, nivel + 1) 
         print(" " * 4 * nivel + "->", nodo.valor) 
-        imprimir_arbol(nodo.izquierda, nivel + 1) 
+        print_arbol(nodo.izquierda, nivel + 1) 
 
 arbol = arbol_binario_aleatorio(8) 
-imprimir_arbol(arbol)
+print_arbol(arbol)
 
